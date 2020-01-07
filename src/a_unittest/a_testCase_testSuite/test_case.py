ed78bf9c313cdd738788a0c5b14639caa957fe1b
Gelomen <kai664792415@qq.com>
2020-01-07 14:42:08 +0800

    format code

--- src/a_unittest/a_testCase_testSuite/test_case.py
+++ src/a_unittest/a_testCase_testSuite/test_case.py
@@ -6,7 +6,7 @@ import unittest
 
 # 被测试类
 
-class Myclass(object):
+class MyClass(object):
     @classmethod
     def sum(cls, a, b):
         return a + b
@@ -18,7 +18,7 @@ class Myclass(object):
 
 # 测试类
 
-class Mytest(unittest.TestCase):
+class MyTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         """初始化类固件"""
@@ -42,11 +42,11 @@ class Mytest(unittest.TestCase):
     # 具体的测试用例，一定要以 test 开头
     def test_sum(self):
         # 断言两数之和是否为 4
-        self.assertEqual(Myclass.sum(self.a, self.b), 4, 'test sum fail')
+        self.assertEqual(MyClass.sum(self.a, self.b), 4, 'test sum fail')
 
     def test_sub(self):
         # 断言两数只差是否为 2
-        self.assertEqual(Myclass.sub(self.a, self.b), 2, 'test sub fail')
+        self.assertEqual(MyClass.sub(self.a, self.b), 2, 'test sub fail')
 
 
 # 启动单元测试
